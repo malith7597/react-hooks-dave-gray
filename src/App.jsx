eefdeb9faf48c6004d1a43bd583f98600c26a620
malithka <malithka@mobitel.lk>
2024-12-30 23:11:25 +0530

    use call back hook created for sum for memoize the function

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useCallback, useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [userInput, setUserInput] = useState("");
+  const [num1] = useState(10);
+  const [num2] = useState(20);
+  const [result, setResult] = useState(0);
 
+  const sum = useCallback(() => {
+    console.log("Sum function called");
+    return num1 + num2;
+  }, [num1, num2]);
+
+  useEffect(() => {
+    console.log("New sum value : ", sum());
+    setResult(sum());
+  }, [sum]);
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main className="App">
+      <input
+        type="text"
+        placeholder="input"
+        value={userInput}
+        onChange={(e) => setUserInput(e.target.value)}
+      />
+      <h1> Output : {userInput || "--"}</h1>
+    </main>
+  );
 }
 
-export default App
+export default App;
