6351a211f2e6d8cee4f753827b9969ed1f36c4e9
malithka <malithka@mobitel.lk>
2024-12-30 22:42:52 +0530

    changed the use states using use Reducer hook

--- src/App.jsx
+++ src/App.jsx
@@ -3,29 +3,62 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "increment":
+      return { ...state, count: state.count + 1 };
+    case "decrement":
+      return { ...state, count: state.count - 1 };
+    case "changeColor":
+      return { ...state, color: !state.color };
+    case "setUserInput":
+      return { ...state, userInput: action.payload };
+    default:
+      return state;
+  }
+};
+
 function App() {
-  const [count, setCount] = useState(0);
+  /* const [count, setCount] = useState(0);
   const [color, setColor] = useState("false");
-  const [userInput, setUserInput] = useState("");
+  const [userInput, setUserInput] = useState(""); */
+
+  const [state, dispatch] = useReducer(reducer, {
+    count: 0,
+    color: false,
+    userInput: ""
+  });
 
   return (
     <main className="App">
       <input
         type="text"
-        value={userInput}
-        onChange={(e) => setUserInput(e.target.value)}
+        /* value={ userInput }  */
+        value={state.userInput}
+        onChange={(e) =>
+          dispatch({ type: "setUserInput", payload: e.target.value })
+        }
       />
       <br />
-      <p>{count}</p>
+      {/*   this can be replaced with the following */}
+      {/*  <p>{count}</p> */}
+      <p>{state.count}</p>
 
       <section
         style={{ display: "flex", justifyContent: "center", gap: "10px" }}
       >
-        <button onClick={() => setCount(count + 1)}>+</button>
-        <button onClick={() => setCount(count - 1)}>-</button>
-        <button onClick={() => setColor(!color)}>Change Color</button>
+        {/* <button onClick={() => setCount(count + 1)}>+</button> */}
+        <button onClick={() => dispatch({ type: "increment" })}>+</button>
+        {/* <button onClick={() => setCount(count - 1)}>-</button> */}
+        <button onClick={() => dispatch({ type: "decrement" })}>-</button>
+        {/*  <button onClick={() => setColor(!color)}>Change Color</button> */}
+        <button onClick={() => dispatch({ type: "changeColor" })}>
+          Change Color
+        </button>
       </section>
-      <p style={{ color: color ? "#FFF" : "#FFF952" }}>{userInput}</p>
+      <p style={{ color: state.color ? "#FFF" : "#FFF952" }}>
+        {state.userInput}
+      </p>
     </main>
   );
 }
